49370ddb5718e8fe9d2ad709dc002aef26fc27cb
myllenaahs <malexandrehs@gmail.com>
2015-05-17 00:48:17 -0300

    hj

---
+++ Calc.java
@@ -0,0 +1,19 @@
+public class Calc {
+
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+	public int subtracao(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplicacao(int a, int b) {
+		return a * b;
+	}
+
+	public int divisao(int a, int b) {
+		return a / b;
+	}
+
+}
--- Principal.java
+++ Principal.java
@@ -1,51 +1,17 @@
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 
 public class Principal {
-	public static void main(String[]args) throws IOException{
-		Scanner key = new Scanner(System.in);
-		
-		double v1,v2,result;
-		String op;
-		result=0;
-		
-		v1=key.nextDouble();
-		op=key.next();
-		v2=key.nextDouble();
-		
-		
-		
-		FileWriter arq = new FileWriter("resultado.txt"); 
-		PrintWriter gravarArq = new PrintWriter(arq);
-		
-		switch (op){
-		case "+":{
-			result=v1+v2;
-			gravarArq.printf("Operação: "+v1+" + "+v2+" = "+result);
-			System.out.println(v1+" + "+v2+" = "+result);
-			break;
-		}
-		case "-":{
-			result=v1-v2;
-			gravarArq.printf("Operação: "+v1+" - "+v2+" = "+result);
-			System.out.println(v1+" - "+v2+" = "+result);
-			break;
-		}
-		case "*":{
-			result=v1*v2;
-			gravarArq.printf("Operação: "+v1+" * "+v2+" = "+result);
-			System.out.println(v1+" * "+v2+" = "+result);
-			break;
-		}
-		case "/":{
-			result=v1/v2;
-			gravarArq.printf("Operação: "+v1+" / "+v2+" = "+result);
-			System.out.println(v1+" / "+v2+" = "+result);
-			break;
-		}
-		}
-		
-		arq.close();
+	public static void main(String args[]) throws IOException {
+		String path = "d:\\arquivo.txt";
+		Scanner in = new Scanner(System.in);
+		int op;
 
+		do {
+			System.out.println("Deseja repetir? 1-Sim/2-Não");
+			op = in.nextInt();
+			Manipulador.escritor(path);
+			Manipulador.leitor(path);
+		} while (op != 2);
 	}
 }
